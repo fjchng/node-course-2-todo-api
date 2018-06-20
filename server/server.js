1c4e3f823b0d8e81c0c84af2e1dbccbb15a02e6e
fjchng <fjchng@fjchng-mac.local>
2018-06-20 10:40:48 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {ObjectID} = require('mongodb')
 
 var app = express()
 
@@ -29,6 +30,19 @@ app.get('/todos', (req, res) => {
 	})
 })
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send('Invalid ID')
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send('ID not found')
+		} else {
+			res.status(200).send({todo})
+		}
+	}).catch((e) => res.status(404).send('Error'))
+})
 
 app.listen(3000, () => {
 	console.log('Started on port 3000')
