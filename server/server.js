d94444c51cf3290d99d04efed7513deccda2bd29
fjchng <fjchng@fjchng-mac.local>
2018-06-25 11:15:59 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -86,6 +86,7 @@ app.patch('/todos/:id', (req, res) => {
 	})
 })
 
+// Create user
 app.post('/users', (req, res) => {
 	var user = new User(_.pick(req.body, ['email', 'password']))
 	user.save().then(() => {
@@ -101,6 +102,17 @@ app.get('/users/me', authenticate, (req, res)=> {
 	res.send(req.user)
 })
 
+// User login
+app.post('/users/login', (req, res) => {
+	var body = new User(_.pick(req.body, ['email', 'password']))
+	User.findByCredentials(body.email, body.password).then((user) => {
+		user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user)
+		})
+	}).catch((e) => {
+		res.status(400).send()
+	})
+})
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`)
